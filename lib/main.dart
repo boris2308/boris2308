bc0d62b6af5d76b0fadea726a3199fd0757f86ee
boris2308 <71342447+boris2308@users.noreply.github.com>
2021-06-10 22:46:04 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:pro/ui/home.dart';
+
+//void main() => runApp(ScaffoldExample());
+
+  void main() => runApp( new MaterialApp(
+
+    home:  MovieListView (),
+
+  ));
---
+++ lib/model/movie.dart
@@ -0,0 +1,29 @@
+class Movie {
+  String _title;
+
+  String get title => _title;
+
+  set title(String title) {
+    _title = title;
+  }
+  String year;
+  String rated;
+  String released;
+  String runtime;
+  String genre;
+  String director;
+  String writer;
+  String actors;
+  String plot;
+  String language;
+  String country;
+  String awards;
+  String poster;
+  String metascore;
+  String imdbRating;
+  String imdbVotes;
+  String imdbID;
+  String type;
+  String response;
+  List<String> images;
+}
